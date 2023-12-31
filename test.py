f19c1c544c6b6ee425763c83ba38e9454e5835ec
fbckdgns3@naver.com <fbckdgns3@naver.com>
2023-12-31 18:40:29 +0900

    fix: test 용 streamlit 기능 추가

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+import streamlit as st
 import cv2
 import mediapipe as mp
 import numpy as np
+import tempfile
 
 # 각도 계산 함수
 def calculate_angle(landmark1, landmark2):
@@ -9,6 +11,7 @@ def calculate_angle(landmark1, landmark2):
     angle_radians = np.arctan2(dy, dx)
     angle_degrees = np.degrees(angle_radians)
     return angle_degrees
+
 
 # 좌표 추출 함수
 def extract_coordinates(results):
@@ -44,10 +47,22 @@ def process_video(video_path):
     cap.release()
     return extracted_coordinates
 
+# streamlit 실행 함수
+def main():
+    st.title("Extract coordinate")
 
-def test_process_video():
-    test_video_path = "path/to/test_video.mp4"
-    results = process_video(test_video_path)
+    uploaded_file = st.file_uploader("Upload a video file", type=["mp4", "avi"])
+
+    if uploaded_file is not None:
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as tmpfile:
+            tmpfile.write(uploaded_file.read())
+            video_path = tmpfile.name
+
+       
+        st.video(video_path)
+        results = process_video(video_path)
+        for coordinates in results:
+            st.write(coordinates)
 
 if __name__ == "__main__":
-    test_process_video()
+    main()
